596fc1510e08a27c98bec6abb6d11b9b9fd23384
Kelly Krans <honeybeebake.mail@gmail.com>
2025-08-10 07:44:24 -0500

    Fix URL import: use 'thanks' view instead of 'order_success'

--- honeybee/urls.py
+++ honeybee/urls.py
@@ -1,9 +1,10 @@
+# honeybee/urls.py
 from django.contrib import admin
 from django.urls import path
-from orderform.views import home, order_success
+from orderform.views import home, thanks  # <- import 'thanks', not 'order_success'
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", home, name="home"),
-    path("success/", order_success, name="order_success"),
+    path("thanks/", thanks, name="thanks"),  # <- route name used by views.py redirect
 ]
