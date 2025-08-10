62b4524f675ceb9878f163e4a0b0f0d0df0d5530
Kelly Krans <honeybeebake.mail@gmail.com>
2025-08-10 12:32:06 -0500

    Make landing page the homepage; move order form to /order/

--- honeybee/urls.py
+++ honeybee/urls.py
@@ -1,10 +1,18 @@
+from django.contrib import admin
 from django.urls import path
-from orderform import views
+from orderform import views as of_views
 
 urlpatterns = [
-    path("", views.home, name="home"),
-    path("thanks/", views.thanks, name="thanks"),
-    path("landing/", views.landing, name="landing"),
-    path("rewards/", views.rewards, name="rewards"),
-    path("rewards/thanks/", views.rewards_thanks, name="rewards_thanks"),
+    path("admin/", admin.site.urls),
+
+    # NEW: Landing page is now the homepage
+    path("", of_views.landing_page, name="landing"),
+
+    # Order flow
+    path("order/", of_views.home, name="home"),
+    path("thanks/", of_views.thanks, name="thanks"),
+
+    # Southern Sweets Club
+    path("rewards/", of_views.rewards, name="rewards"),
+    path("rewards/thanks/", of_views.rewards_thanks, name="rewards_thanks"),
 ]
